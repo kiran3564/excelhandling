854d856cc4b73e0e4ea6c61bfe684d5e58a10595
sai kiran <kiran3564@gmail.com>
2025-06-04 15:08:42 +0000

    fafadf

--- crt-excel-handling-main/crt-excel-handling-main/libraries/GitOperations.py
+++ crt-excel-handling-main/crt-excel-handling-main/libraries/GitOperations.py
@@ -1,30 +1,41 @@
-# File: GitOperations.py
-
 import os
-import git
 from robot.api import logger
 from robot.api.deco import keyword
+import git
 
+class GitOperations(object):
 
-class GitOperations:
-    
     def __init__(self):
+        self._project_name = str(os.environ.get("SCRIPTS"))
+
+        # Execution path is different in normal test runs and in Live Testing,
+        # and we need to take that into account
         self._project_path = os.getcwd()
-        self._data_path = os.path.join(self._project_path, "data")
-        logger.console(f"[INIT] Project Path: {self._project_path}")
-        logger.console(f"[INIT] Data Path: {self._data_path}")
 
-    @keyword("Commit And Push")
+        # SCRIPTS env variable contains the suite name (except locally)
+        if self._project_name != "None":
+            if self._project_path == "/home/services/suite/tests":
+                # We are in live testing and project name is not in project path
+                self._project_path = os.path.join("/home/services/suite/")
+            else:
+                self._project_path = os.path.join(os.getcwd(), self._project_name)
+
+        logger.console(self._project_path)
+        self._data_path = os.path.join(self._project_path, "data/")
+        logger.console(self._data_path)
+
+    @keyword
     def commit_and_push(self, file_name, git_branch):
+
         path_to_file = os.path.join(self._data_path, file_name)
-        logger.console(f"[INFO] File to commit: {path_to_file}")
 
-        repo = git.Repo(self._project_path)
+        # Repo exists in project path
+        my_repo = git.Repo(self._project_path)
 
-        logger.console(f"[GIT STATUS BEFORE]\n{repo.git.status()}")
-        
-        repo.index.add([path_to_file])
-        repo.index.commit(f"Robot Framework: Updated {file_name}")
-        repo.git.push("origin", git_branch)
+        # Print git status to console for visibility
+        logger.console("\n" + my_repo.git.status() + "\n")
 
-        logger.console("[SUCCESS] Commit and push completed.")
+        # Add, commit and push to git
+        my_repo.index.add(path_to_file)
+        my_repo.index.commit("CRT robot committing changes to {}".format(file_name))
+        my_repo.git.push("origin", git_branch)
